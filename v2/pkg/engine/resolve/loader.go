c7939746856d79301edaa570b0660ab738e8c9c9
Gio Gutierrez <giovanny.gutierrez@commure.com>
2022-12-23 13:01:27 -0500

    feat: Add transformation option to the field configuration

--- v2/pkg/engine/resolve/loader.go
+++ v2/pkg/engine/resolve/loader.go
@@ -576,7 +576,9 @@ func (l *Loader) mergeResult(fetchItem *FetchItem, res *result, items []*astjson
 		if value.Type() != astjson.TypeObject {
 			return l.renderErrorsFailedToFetch(fetchItem, res, invalidGraphQLResponseShape)
 		}
-		l.resolvable.data = value
+		if l.resolvable.data == nil {
+			l.resolvable.data = value
+		}
 		return nil
 	}
 	if len(items) == 1 && res.batchStats == nil {
@@ -591,7 +593,11 @@ func (l *Loader) mergeResult(fetchItem *FetchItem, res *result, items []*astjson
 		return nil
 	}
 	batch := value.GetArray()
-	if batch == nil {
+	if len(batch) == 0 {
+		return nil
+	}
+
+	if len(batch) != len(items) {
 		return l.renderErrorsFailedToFetch(fetchItem, res, invalidGraphQLResponseShape)
 	}
 	if res.batchStats != nil {
