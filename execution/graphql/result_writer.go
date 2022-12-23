53682d907a6db2cb6a0731a128d776f126a49f9a
Gio Gutierrez <giovanny.gutierrez@commure.com>
2022-12-23 13:01:27 -0500

    feat: Add transformation option to the field configuration

--- execution/graphql/result_writer.go
+++ execution/graphql/result_writer.go
@@ -13,8 +13,9 @@ import (
 )
 
 type EngineResultWriter struct {
-	buf           *bytes.Buffer
-	flushCallback func(data []byte)
+	buf              *bytes.Buffer
+	flushCallback    func(data []byte)
+	completeCallback func()
 }
 
 var _ resolve.SubscriptionResponseWriter = (*EngineResultWriter)(nil)
@@ -32,7 +33,9 @@ func NewEngineResultWriterFromBuffer(buf *bytes.Buffer) EngineResultWriter {
 }
 
 func (e *EngineResultWriter) Complete() {
-
+	if e.completeCallback != nil {
+		e.completeCallback()
+	}
 }
 
 func (e *EngineResultWriter) Heartbeat() error {
@@ -45,6 +48,10 @@ func (e *EngineResultWriter) Close(_ resolve.SubscriptionCloseKind) {
 
 func (e *EngineResultWriter) SetFlushCallback(flushCb func(data []byte)) {
 	e.flushCallback = flushCb
+}
+
+func (e *EngineResultWriter) SetCompleteCallback(completeCb func()) {
+	e.completeCallback = completeCb
 }
 
 func (e *EngineResultWriter) Write(p []byte) (n int, err error) {
