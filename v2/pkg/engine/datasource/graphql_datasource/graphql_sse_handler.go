1b8a2646073d14b7e2be51ffaecab729564990bc
Gio Gutierrez <giovanny.gutierrez@commure.com>
2022-12-23 13:01:27 -0500

    feat: Add transformation option to the field configuration

--- v2/pkg/engine/datasource/graphql_datasource/graphql_sse_handler.go
+++ v2/pkg/engine/datasource/graphql_datasource/graphql_sse_handler.go
@@ -45,13 +45,15 @@ func newSSEConnectionHandler(requestContext, engineContext context.Context, conn
 func (h *gqlSSEConnectionHandler) StartBlocking() {
 	dataCh := make(chan []byte)
 	errCh := make(chan []byte)
+	complete := make(chan bool)
 	defer func() {
+		close(complete)
 		close(dataCh)
 		close(errCh)
 		h.updater.Complete()
 	}()
 
-	go h.subscribe(dataCh, errCh)
+	go h.subscribe(dataCh, errCh, complete)
 
 	for {
 		select {
@@ -59,6 +61,8 @@ func (h *gqlSSEConnectionHandler) StartBlocking() {
 			h.updater.Update(data)
 		case data := <-errCh:
 			h.updater.Update(data)
+			return
+		case <-complete:
 			return
 		case <-h.requestContext.Done():
 			return
@@ -68,7 +72,7 @@ func (h *gqlSSEConnectionHandler) StartBlocking() {
 	}
 }
 
-func (h *gqlSSEConnectionHandler) subscribe(dataCh, errCh chan []byte) {
+func (h *gqlSSEConnectionHandler) subscribe(dataCh, errCh chan []byte, complete chan<- bool) {
 	resp, err := h.performSubscriptionRequest()
 	if err != nil {
 		h.log.Error("failed to perform subscription request", log.Error(err))
@@ -136,6 +140,7 @@ func (h *gqlSSEConnectionHandler) subscribe(dataCh, errCh chan []byte) {
 
 				switch {
 				case bytes.Equal(event, eventTypeComplete):
+					complete <- true
 					return
 				case bytes.Equal(event, eventTypeNext):
 					continue
