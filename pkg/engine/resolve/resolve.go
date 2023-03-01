99afeb3e5d4cd14560b827cfc3a8e08efd3345f1
Gio Gutierrez <giovanny.gutierrez@commure.com>
2023-03-01 08:58:08 -0500

    feat: Use CompleteFlushWriter

--- pkg/engine/resolve/resolve.go
+++ pkg/engine/resolve/resolve.go
@@ -614,8 +614,8 @@ func (r *Resolver) ResolveGraphQLSubscription(ctx *Context, subscription *GraphQ
 		case <-resolverDone:
 			return nil
 		case <-completed:
-			if close, ok := writer.(io.WriteCloser); ok {
-				return close.Close()
+			if f, ok := writer.(CompleteFlushWriter); ok {
+				return f.Complete()
 			}
 			return nil
 		case data, ok := <-next:
@@ -1672,6 +1672,10 @@ type GraphQLSubscriptionTrigger struct {
 type FlushWriter interface {
 	io.Writer
 	Flush()
+}
+
+type CompleteFlushWriter interface {
+	Complete() error
 }
 
 type GraphQLResponse struct {
